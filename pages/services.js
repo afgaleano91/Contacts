3e64499b04633dfb031161a34a26cd4348ae5ed6
Andres Galeano <afgaleano91@misena.edu.co>
2020-09-08 01:33:46 -0500

    Uso de Next con promesas

---
+++ pages/services.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Container from "../components/container";
+
+const Services = () => (
+    <Container>
+        <Head>
+            <title>Next.js Project - Services</title>
+        </Head>
+        <h1>Services</h1>
+        <p>
+            Nostrud sunt mollit laborum aliquip reprehenderit pariatur dolore aute cillum nulla. Amet tempor qui anim aliqua do adipisicing cillum nulla consectetur do enim. Aute sit do cupidatat veniam exercitation sunt. Sit adipisicing aliqua qui consequat. Veniam consectetur eu sunt minim veniam qui deserunt non ex nulla sunt ipsum in.
+        </p>
+    </Container>
+)
+
+export default Services;
